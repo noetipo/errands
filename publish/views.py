2b367b7aacb7fbc6dbc9bc59dd632f2ced3711cb
noeTipo <noe.tipo@gmail.com>
2017-11-26 18:43:49 -0500

    pagination demo

--- publish/views.py
+++ publish/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+import logging
 
+from backend_utils.logs import log_params
+from backend_utils.pagination import ModelPagination
 # Create your views here.
 
 
@@ -10,10 +13,11 @@ from rest_framework_mongoengine.viewsets import ModelViewSet as MongoModelViewSe
 from publish.serializers import *
 from publish.models import Author
 
-class AuthorViewSet(MongoModelViewSet):
+class AuthorViewSet(ModelPagination,MongoModelViewSet):
+    queryset=Author.objects.all()
     lookup_field = 'id'
     serializer_class = AuthorSerializer
 
-    def get_queryset(self):
-        return Author.objects.all()
+    #def get_queryset(self):
+    #    return Author.objects.all()
 
